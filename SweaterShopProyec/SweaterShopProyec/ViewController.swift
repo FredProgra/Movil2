325b94ba93e2da5011e1b09d03383475d2ef9e21
DAMII <damii@iMACBE207-ST16.local>
2024-04-21 15:00:30 -0500

    segundo commit

--- SweaterShopProyec/SweaterShopProyec/ViewController.swift
+++ SweaterShopProyec/SweaterShopProyec/ViewController.swift
@@ -11,25 +11,40 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var pages: UIPageControl!
-    @IBOutlet weak var mycollectionview: UICollectionView!
-    var imageArray = ["sweater_white","swaters_verde"]
+    @IBOutlet weak var mycollectionView: UICollectionView!
+    var imageArray = ["sweaters_verde","sweater_white"]
+    var index = 0
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(scrollingsetup), userInfo: nil, repeats: true)
     }
-
+    @objc func scrollingsetup(){
+        if index < imageArray.count - 1{
+            index = index + 1
+        }else{
+            index = 0
+        }
+        pages.numberOfPages = imageArray.count
+        pages.currentPage = index
+        mycollectionView.scrollToItem(at: IndexPath(item: index, section: 0), at: .right, animated: true)
+    }
 }
 
 extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        <#code#>
+        return imageArray.count
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as? CustomCollectionViewCell
+        cell?.adsImage.image = UIImage(named: imageArray[indexPath.row])
+        cell?.layer.borderWidth = 1
+        cell?.layer.borderColor = UIColor.white.cgColor
+        cell?.layer.cornerRadius = 20
+        return cell!
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        <#code#>
+        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
     }
 }
