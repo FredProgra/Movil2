60f4078eed09d2163f3628b8766616c432ea7127
¨AngeloGuio¨ <guioangelo.garcia@gmail.com>
2024-05-11 11:39:03 -0500

    l

---
+++ SweaterShopProyec/SweaterShopProyec/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  SweaterShopProyec
+//
+//  Created by DAMII on 11/05/24.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+   
+    
+    @IBOutlet weak var sweaterImg: UIImageView!
+    
+    @IBOutlet weak var sweaterDetalle: UILabel!
+    
+}
--- SweaterShopProyec/SweaterShopProyec/ViewController.swift
+++ SweaterShopProyec/SweaterShopProyec/ViewController.swift
@@ -10,41 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     
-    @IBOutlet weak var pages: UIPageControl!
-    @IBOutlet weak var mycollectionView: UICollectionView!
-    var imageArray = ["sweaters_verde","sweater_white"]
-    var index = 0
     
+    @IBOutlet weak var collectionView: UICollectionView!
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(scrollingsetup), userInfo: nil, repeats: true)
+        
     }
-    @objc func scrollingsetup(){
-        if index < imageArray.count - 1{
-            index = index + 1
-        }else{
-            index = 0
-        }
-        pages.numberOfPages = imageArray.count
-        pages.currentPage = index
-        mycollectionView.scrollToItem(at: IndexPath(item: index, section: 0), at: .right, animated: true)
-    }
-}
-
-extension ViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageArray.count
-    }
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCollectionViewCell", for: indexPath) as? CustomCollectionViewCell
-        cell?.adsImage.image = UIImage(named: imageArray[indexPath.row])
-        cell?.layer.borderWidth = 1
-        cell?.layer.borderColor = UIColor.white.cgColor
-        cell?.layer.cornerRadius = 20
-        return cell!
-    }
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
-    }
+   
 }
